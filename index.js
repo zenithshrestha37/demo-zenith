c45467a1daaa05450269d5cd4eb7fad0449106f2
zenithshrestha37 <zenithshrestha37@gmail.com>
2021-07-26 17:24:04 -0500

    app use

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const path = require('path')
 const app = express()
 
-
+app.use(express.static("public"))
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,'/public/index.html'))
